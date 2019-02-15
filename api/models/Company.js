78cbb9bffb0c45a5153671048ac42bd836201eb5
unknown <wanderson.rocha@oseusonho.com.br>
2019-02-15 17:14:41 -0300

    troca de numbre p String

--- api/models/Company.js
+++ api/models/Company.js
@@ -23,7 +23,7 @@ module.exports = {
       type: 'string', maxLength: 120
     },
     telefone: {
-      type: 'string'
+      type: 'string', allowNull: true
     },
     site: {
       type: 'string'
