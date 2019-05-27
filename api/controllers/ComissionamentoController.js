b21240fd991c2ac667ab117296b55ac6920aa954
Wanderson Silva <wanderson.silva@atpse.ad.corp>
2019-05-27 11:22:01 -0300

    nova controller

---
+++ api/controllers/ComissionamentoController.js
@@ -0,0 +1,22 @@
+/**
+ * ComissionamentoController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+    valida_comissionamento: function(req, res){
+        var x = req.param('comissionamento_id');
+        var y = req.param('user_id');
+
+        Comissionamento.query('CALL prc_valida_comissionamento("'+x+'","'+y+'")', function(err, rawResult) {
+            if (err) { return res.serverError(err); }          
+            // sails.log(rawResult);
+            
+            return res.ok();
+          
+          });
+    }
+};
+
