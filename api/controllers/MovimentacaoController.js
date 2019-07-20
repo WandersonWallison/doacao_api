4e23878a8f87b49cb3f11da0192f191b79001d03
unknown <wandersonwallison@gmail.com>
2019-07-20 11:14:30 -0300

    add coalesce na query

--- api/controllers/MovimentacaoController.js
+++ api/controllers/MovimentacaoController.js
@@ -9,10 +9,9 @@ module.exports = {
     calcula_movimentacao: function(req, res){
         var x = req.param('user_id');
 
-        
-        
-        Movimentacao.query('select ((SELECT sum(valor) FROM prosp_dev.movimentacao where id_cliente = "'+x+'" and id_tipo_movimentacao = 1  and ativo = 1)'+ '-' +
-            '(SELECT sum(valor) FROM prosp_dev.movimentacao where id_cliente = "'+x+'" and id_tipo_movimentacao in (2,3) and id_situacao_movimento <> 7 and ativo = 1))', function(err, rawResult) {
+    
+        Movimentacao.query('select ((SELECT coalesce(sum(valor),0) FROM prosp_dev.movimentacao where id_cliente = "'+x+'" and id_tipo_movimentacao = 1  and ativo = 1)'+ '-' +
+            '(SELECT coalesce(sum(valor),0)FROM prosp_dev.movimentacao where id_cliente = "'+x+'" and id_tipo_movimentacao in (2,3) and id_situacao_movimento <> 7 and ativo = 1))', function(err, rawResult) {
             if (err) { return res.serverError(err); }
           
             // sails.log(rawResult);
